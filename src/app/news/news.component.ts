723d293e0ea479362e817674b5a1034e287589cc
Singh, Rajat <RSing138@ITS.JNJ.com>
2020-05-03 17:41:28 +0530

    Loading spinner

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -7,12 +7,16 @@ import { NewsService } from './news.service';
   styleUrls: ['./news.component.css']})
 export class NewsComponent implements OnInit {
 
+  showLoadingSpinner = false;
+
   news
   constructor(private newsService: NewsService) { }
 
   ngOnInit(): void {
+    this.showLoadingSpinner = true;
     this.newsService.getNews().subscribe((news)=>{
          this.news=news;
+         this.showLoadingSpinner = false;
     });
   }
 
